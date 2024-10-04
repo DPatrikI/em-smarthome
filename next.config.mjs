6736ab1568d7c51ff75e68f66df19de86f9f9e97
Patrik Dóczy <doczy.patrik410@gmail.com>
2024-10-04 19:58:30 +0200

    Added env setup to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        COSMOS_DB_ENDPOINT: process.env.COSMOS_DB_ENDPOINT,
+        COSMOS_DB_KEY: process.env.COSMOS_DB_KEY,
+        COSMOS_DB_DATABASE: process.env.COSMOS_DB_DATABASE,
+        COSMOS_DB_CONTAINER: process.env.COSMOS_DB_CONTAINER,
+    },
+};
 
 export default nextConfig;
